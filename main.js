bb350ebf1ba51acf9d036f350cbe4710622d96ca
Nagore02 <1ebe2.nagore.santelices@gmail.com>
2024-12-08 13:24:43 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,126 @@
+(function ($) {
+    "use strict";
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Date and time picker
+    $('#date').datetimepicker({
+        format: 'L'
+    });
+    $('#time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Service carousel
+    $(".service-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        loop: true,
+        dots: false,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
+
+
+    // Pricing carousel
+    $(".pricing-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        loop: true,
+        dots: false,
+        nav : false,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        margin: 30,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+    
+})(jQuery);
+
+function toggleMarca(elemento) {
+  const icono = elemento.querySelector('i'); // Seleccionamos el icono dentro del div
+  
+  // Alternamos entre 'far fa-circle' y 'fas fa-check-circle'
+  if (icono.classList.contains('far')) {
+    icono.classList.remove('far', 'fa-circle');
+    icono.classList.add('fas', 'fa-check-circle');
+  } else {
+    icono.classList.remove('fas', 'fa-check-circle');
+    icono.classList.add('far', 'fa-circle');
+  }
+}
+
+
